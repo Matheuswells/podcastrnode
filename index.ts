b0987e095cd8af32eacaf95d5b32060545699d32
Matheus Wells <36707321+Matheuswells@users.noreply.github.com>
2021-04-27 22:27:23 -0300

    default port nad public folder on build

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import app from './src/app'
-require('dotenv').config()
 
-
-app.listen(process.env.PORT,() => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`)
+app.listen(80,() => {
+  console.log(`Server running on http://localhost:80`)
 })

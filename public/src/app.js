afbcc0c95e159bfc933e605d1586232c026ec26d
Matheus Wells <36707321+Matheuswells@users.noreply.github.com>
2021-04-27 22:23:59 -0300

    asd

--- public/src/app.js
+++ public/src/app.js
@@ -1,9 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var fs = require("fs");
+var path = require("path");
 var app = express();
+var rawdata = String(fs.readFileSync(path.resolve(__dirname, 'server.json')));
+var data = JSON.parse(rawdata);
 app.get('/', function (req, res) {
-    res.send(200);
+    res.json(data);
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
